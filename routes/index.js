e4cb2cf7ec9e2310895d295c82aa350329d993c3
Aquiles Mariani <aquilesmariani@gmail.com>
2018-10-15 18:32:53 -0300

    filter artist

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,17 @@ const upResponse = (req, res) => {
 }
 
 const getArtists = (callback, resp) => {
-  return http.get('http://ai.bakinglyrics.com:8080/api/v1/artists/?page=1&per_page=10', (res) => {
-    
+  const { filter } = callback.query
+  const url = filter
+    ? `http://ai.bakinglyrics.com:8080/api/v1/artists/search/${filter}`
+    : 'http://ai.bakinglyrics.com:8080/api/v1/artists/?page=1&per_page=10'
+  return http.get(url, (res) => {
+
     const { statusCode } = res;
     const contentType = res.headers['content-type'];
 
     let error;
+    if (statusCode === 404) resp.status(404).send('Artist not found')
     if (statusCode !== 200) {
       error = new Error('Request Failed.\n' +
                         `Status Code: ${statusCode}`);
@@ -31,7 +36,7 @@ const getArtists = (callback, resp) => {
 
     res.setEncoding('utf8');
     let rawData = '';
-    
+
     res.on('data', (chunk) => { rawData += chunk; });
     res.on('end', () => {
       try {
@@ -47,6 +52,7 @@ const getArtists = (callback, resp) => {
   });
 
 }
+
 
 router.get('/', upResponse)
 router.get('/artists', getArtists)
