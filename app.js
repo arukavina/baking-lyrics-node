2ce6ba84b82775a83d12ef30965785d1ebef05e8
Aquiles Mariani <aquilesmariani@gmail.com>
2018-10-16 16:01:38 -0300

    allow cross-origin domain calls

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const port = config.PORT
 var app = express();
 var router = require('./routes')
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', router)
 
 app.listen(port, function () {
