d54b6149a7bf8986d90cebd497a73cbea6766d76
Aquiles Mariani <aquilesmariani@gmail.com>
2018-10-18 01:56:08 -0300

    api call to generate song

--- routes/index.js
+++ routes/index.js
@@ -53,8 +53,66 @@ const getArtists = (callback, resp) => {
 
 }
 
+const generateSong = (callback, resp) => {
+  const { seed, number_words, artist_id } = callback.query
+  if (!seed || !number_words || !artist_id) return resp.status(400).send({error: 'Missing parameter'})
+  const url = `http://ai.bakinglyrics.com:8080/api/v1/artificial_songs/generate/en/${seed}/${number_words}/artist/${artist_id}/`
+
+  return get(url, { seed, number_words, artist_id }, resp)
+
+}
+
+const getGeneratedSong = (callback, resp) => {
+  const { artificial_song_id } = callback.query
+  if (!artificial_song_id) return resp.status(400).send({error: 'Missing parameter'})
+  const url = `http://ai.bakinglyrics.com:8080/api/v1/artificial_songs/${artificial_song_id}`
+
+  return get(url, {artificial_song_id}, resp)
+
+}
+
+const get = (url, query, resp) => {
+
+  return http.get(url, (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+    }
+    if (error) {
+      console.error(error.message);
+      // consume response data to free up memory
+      res.resume();
+      return;
+    }
+
+    res.setEncoding('utf8');
+    let rawData = '';
+
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        return resp.status(200).send(parsedData)
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
+
+}
 
 router.get('/', upResponse)
 router.get('/artists', getArtists)
+router.get('/generate', generateSong)
+router.get('/generated-song', getGeneratedSong)
 
 module.exports = router
